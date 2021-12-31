9f5140a57784a3183c117d42fec937e88c9e14bc
bbebe <96916285+bbebe@users.noreply.github.com>
2021-12-31 21:26:29 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,17 +1,17 @@
 const textConfig = {
   text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text2: "Tớ có điều này muốn nói với cậu.",
+  text3: "Cậu đã sẵn sàng chưa?",
+  text4: "",
+  text5: "chưa",
+  text6: "rồi !!!",
+  text7: "2021 của cậu thế nào?",
+  text8: "gửi cậu nè",
+  text9: "tớ đã rất cố gắng!",
+  text10: "2021 cậu đã rất cố gắng rồi",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
-  text12: "Okii lunn <3",
+    "Cảm ơn cậu rất nhiều vì đã ở cùng Fercaline suốt năm 2021. Chúc cậu 2022 thật vui vẻ và hạnh phúc nha!",
+  text12: "2022 sẽ yêu thương tớ!",
 };
 
 $(document).ready(function () {
